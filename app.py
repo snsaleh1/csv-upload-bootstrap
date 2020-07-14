24243e6a082dc70ca4b80e91cbe7677f2bd47ebc
snsaleh1 <snsaleh11@gmail.com>
2020-07-13 20:08:47 -0500

    organized table with fieldnames

--- app.py
+++ app.py
@@ -11,9 +11,18 @@ def root():
     if request.method == 'GET':
         return render_template('home.html')
     elif request.method=='POST':
-        user_csv = request.form.get('user_csv')
-        print(user_csv)
+        results = []
 
+        user_csv = request.form.get('user_csv').split('\n')
+        reader = csv.DictReader(user_csv)
+
+        for row in reader:
+            results.append(dict(row))
+
+        fieldnames = [key for key in results[0].keys()]
+
+        return render_template('home.html', results=results, fieldnames=fieldnames, len=len)
+        
 if __name__ =='__main__':
     app.run(debug=True)
 
